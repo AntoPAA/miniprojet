3359bac30b28e3171ee7c52173c2243e12d9cd2e
Antonin Pasquier <antonin.pasquier45@gmail.com>
2023-11-30 15:31:38 +0100

    database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,19 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+CREATE TABLE categorie (
+  id INTEGER PRIMARY KEY NOT NULL,
+  language VARCHAR(255),
+  country VARCHAR(255)
 );
+
+INSERT INTO categorie(id,language,country) VALUES (1,'Chinois','Chine');
+INSERT INTO categorie(id,language,country) VALUES (2,'Italien','Italy');
+
+CREATE TABLE city (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  image VARCHAR(255),
+  categorie_id INTEGER,
+  FOREIGN KEY (categorie_id) REFERENCES categorie(id)
+);
+
+INSERT INTO city(id,name,description,image,categorie_id) VALUE (1,'Pekin','très grande ville situé en chine','tkt',1);
